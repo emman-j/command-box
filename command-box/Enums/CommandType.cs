c6e64b6490fda30827bffff8d6f4f69e872ee350
emman-j <176351546+emman-j@users.noreply.github.com>
2026-01-12 13:53:58 +0800

    added enum types for possible script types

---
+++ command-box/Enums/CommandType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_box.Enums
+{
+    public enum CommandType
+    {
+        Batch,
+        PowerShell,
+        Shell,
+        Python,
+        Node,
+    }
+}
