66879970af33fe94e65ae20cfeb0107bd13acffa
emman-j <176351546+emman-j@users.noreply.github.com>
2026-01-12 15:55:46 +0800

    test of initial implementation

--- command-box/Program.cs
+++ command-box/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            CommandsManager commandsManager = new CommandsManager();
+
+            bool running = true;
+
+            while (running)
+            { 
+                if(!running)
+                    break;
+
+                string[] args = Console.ReadLine().Split(' ');
+                string command = args[0];
+
+                switch(command.ToLower())
+                {
+                    case "-h":
+                    case "help":
+                        commandsManager.ShowHelp();
+                        continue;
+                    case "exit":
+                    case "quit":
+                    case "-q":
+                        running = false;
+                        continue;
+                }
+
+                string[] commandArgs = args.Skip(1).ToArray();
+                commandsManager.ExecuteCommand(command, commandArgs);
+            }
         }
     }
 }
