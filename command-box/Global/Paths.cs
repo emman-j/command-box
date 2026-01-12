c466c374b28b085093542a117e1e72bc33f66d31
emman-j <176351546+emman-j@users.noreply.github.com>
2026-01-12 13:53:42 +0800

    added global paths for easy path management

---
+++ command-box/Global/Paths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_box.Global
+{
+    public static class Paths
+    {
+        //Root path for the application folder
+        public static string AppDataPath { get => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "command-box"); }
+        public static string DataDir { get => Path.Combine(AppDataPath, "data"); }
+        public static string SettingDir { get => Path.Combine(AppDataPath,"settings"); }
+        public static string ScriptsDir { get => Path.Combine(AppDataPath, "scripts"); }
+        public static string LogsDir { get => Path.Combine(DataDir, "logs"); }
+        public static string ErrorLogsDir { get => Path.Combine(LogsDir, "error logs"); }
+        public static string ConsoleLogsDir { get => Path.Combine(LogsDir, "console logs"); }
+    }
+}
