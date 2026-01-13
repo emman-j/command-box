4fe7f0c5e855600d0e08eb4a7b6211ee44e5beee
emman-j <176351546+emman-j@users.noreply.github.com>
2026-01-13 10:38:28 +0800

    add cache and history file path

--- command-box/Global/Paths.cs
+++ command-box/Global/Paths.cs
@@ -10,11 +10,17 @@ namespace command_box.Global
     {
         //Root path for the application folder
         public static string AppDataPath { get => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "command-box"); }
+
+        // Directoies within the application folder
         public static string DataDir { get => Path.Combine(AppDataPath, "data"); }
         public static string SettingsDir { get => Path.Combine(AppDataPath,"settings"); }
         public static string ScriptsDir { get => Path.Combine(AppDataPath, "scripts"); }
         public static string LogsDir { get => Path.Combine(DataDir, "logs"); }
         public static string ErrorLogsDir { get => Path.Combine(LogsDir, "error logs"); }
         public static string ConsoleLogsDir { get => Path.Combine(LogsDir, "console logs"); }
+
+        //Filepaths
+        public static string Cache { get => Path.Combine(DataDir, "cache"); }
+        public static string History { get => Path.Combine(DataDir, "history"); }
     }
 }
