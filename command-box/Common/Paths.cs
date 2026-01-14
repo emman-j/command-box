a4de22c0a6f9320fec8d404eab86b8aa257467bc
emman-j <176351546+emman-j@users.noreply.github.com>
2026-01-14 09:33:30 +0800

    renamed global folder to common

--- command-box/Common/Paths.cs
+++ command-box/Common/Paths.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace command_box.Global
+﻿namespace command_box.Common
 {
     public static class Paths
     {
